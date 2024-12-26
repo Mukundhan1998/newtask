3ab95bc6f14bfd95f72099c56cd2f9d76f6ad2c6
Mukundhan1998 <mukundhanr98@gmail.com>
2024-12-26 19:05:59 +0000

    commit with build script

--- tasks.sh
+++ tasks.sh
@@ -8,7 +8,7 @@ echo "Enter the second number:"
 read num2
 
 # Perform subtraction
-result=$((num1 - num2))
+result=$((num1 - num2)+3)
 
 # Display the result
 echo "The result of $num1 - $num2 is: $result"
