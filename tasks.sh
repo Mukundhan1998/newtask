08860ddf6bcb133b98d8e7668af15ba60c3039fd
Mukundhan1998 <mukundhanr98@gmail.com>
2024-12-27 08:43:19 +0000

    again a change

--- tasks.sh
+++ tasks.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+
 
 aa2# This script performs the subtraction#!/bin/bash
 
 # Define numbers (replace with environment variables if needed)
 num1=5
 num2=10
-num3=15
+num3=5
 
 # Perform the operation
 result=$((num1 + num2 - num3))
