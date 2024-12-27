124a1a875df9722fee66a155640d2c5f4fbdbfe4
Mukundhan1998 <mukundhanr98@gmail.com>
2024-12-27 08:04:27 +0000

    commit change has been made

--- tasks.sh
+++ tasks.sh
@@ -7,8 +7,11 @@ read num1
 echo "Enter the second number:"
 read num2
 
+echo "enter the third number"
+read num3
+
 # Perform subtraction
-result=$((num1 - num2)+3)
+result=$((num1 - num2 + num3))
 
 # Display the result
 echo "The result of $num1 - $num2 is: $result"
